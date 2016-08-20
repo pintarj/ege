fbb78ed742eb15d6a9025f66cf0b915d30e7168a
Janez Pintar <pintarj.zero@gmail.com>
2016-08-20 23:27:21 +0200

    added function to create a translated matrix

--- private/include/ege_private/graphic/geometry/matrix.hxx
+++ private/include/ege_private/graphic/geometry/matrix.hxx
@@ -75,6 +75,26 @@ namespace ege_private
                                         matrix[ 15 ] = 1.0f;
                                 }
 
+                                static inline void translate( float* matrix, float x, float y, float z )
+                                {
+                                        matrix[  0 ] = 1.0f;
+                                        matrix[  1 ] = 0.0f;
+                                        matrix[  2 ] = 0.0f;
+                                        matrix[  3 ] = 0.0f;
+                                        matrix[  4 ] = 0.0f;
+                                        matrix[  5 ] = 1.0f;
+                                        matrix[  6 ] = 0.0f;
+                                        matrix[  7 ] = 0.0f;
+                                        matrix[  8 ] = 0.0f;
+                                        matrix[  9 ] = 0.0f;
+                                        matrix[ 10 ] = 1.0f;
+                                        matrix[ 11 ] = 0.0f;
+                                        matrix[ 12 ] = x;
+                                        matrix[ 13 ] = y;
+                                        matrix[ 14 ] = z;
+                                        matrix[ 15 ] = 1.0f;
+                                }
+
                                 static inline void rotate( float* matrix, float radians, float x, float y, float z )
                                 {
                                         float sin       = std::sin( radians );

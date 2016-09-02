4ca8551f2695455ed267ac1e1c54cdf3e68e9f61
Janez Pintar <pintarj.zero@gmail.com>
2016-09-02 17:44:20 +0200

    targetable for rendering class added

---
+++ include/ege/graphic/render/targetable.hxx
@@ -0,0 +1,28 @@
+
+
+#ifndef EGE_GRAPHIC_RENDER_TARGETABLE_HXX
+#define EGE_GRAPHIC_RENDER_TARGETABLE_HXX
+
+
+#include <cstddef>
+
+
+namespace ege
+{
+        namespace graphic
+        {
+                namespace render
+                {
+                        class Targetable
+                        {
+                                public:
+                                        virtual void getDimensions( size_t* width, size_t* height ) = 0;
+                                        virtual size_t getFrameBufferId() = 0;
+                                        virtual ~Targetable() {};
+                        };
+                }
+        }
+}
+
+
+#endif

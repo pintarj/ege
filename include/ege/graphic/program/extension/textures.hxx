b9bf0db9a6eb6dc2d7f42a710b0e05b874e29f4e
Janez Pintar <pintarj.zero@gmail.com>
2016-09-05 14:00:36 +0200

    create program extension for manage texture

---
+++ include/ege/graphic/program/extension/textures.hxx
@@ -0,0 +1,34 @@
+
+
+#ifndef EGE_GRAPHIC_PROGRAM_EXTENSION_TEXTURES_HXX
+#define EGE_GRAPHIC_PROGRAM_EXTENSION_TEXTURES_HXX
+
+
+#include <ege/graphic/program/extension.hxx>
+
+
+namespace ege
+{
+        namespace graphic
+        {
+                namespace program
+                {
+                        namespace extension
+                        {
+                                class Textures: Extension
+                                {
+                                        private:
+                                                size_t* texturesLocation;
+
+                                        protected:
+                                                Textures( Program& program, const char** textureNames, size_t count );
+                                                virtual ~Textures();
+                                                void uniformTextureAt( size_t index, size_t value );
+                                };
+                        }
+                }
+        }
+}
+
+
+#endif

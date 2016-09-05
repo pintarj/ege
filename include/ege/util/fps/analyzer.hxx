461568ce62a80a852bb6f1b10ff9934eeb50627f
Janez Pintar <pintarj.zero@gmail.com>
2016-09-05 15:38:23 +0200

    fps analyzer added

---
+++ include/ege/util/fps/analyzer.hxx
@@ -0,0 +1,33 @@
+
+
+#ifndef EGE_UTIL_FPS_ANALYZER_HXX
+#define EGE_UTIL_FPS_ANALYZER_HXX
+
+
+#include <chrono>
+
+
+namespace ege
+{
+        namespace util
+        {
+                namespace fps
+                {
+                        class Analyzer
+                        {
+                                private:
+                                        std::chrono::time_point<std::chrono::system_clock> lastTimePoint;
+                                        std::chrono::duration< float > lastDelta;
+
+                                public:
+                                        Analyzer( float lastDelta = 0.0f );
+                                        void markTimePoint();
+                                        void setLastDelta( float lastDelta );
+                                        float getLastDelta();
+                        };
+                }
+        }
+}
+
+
+#endif
---
+++ source/util/fps/analyzer.cxx
@@ -0,0 +1,31 @@
+#include <ege/util/fps/analyzer.hxx>
+
+
+using namespace ege::util::fps;
+
+
+Analyzer::Analyzer( float lastDelta )
+{
+        lastTimePoint = std::chrono::system_clock::now();
+        setLastDelta( lastDelta );
+}
+
+
+void Analyzer::markTimePoint()
+{
+        std::chrono::time_point< std::chrono::system_clock > currentTimePoint = std::chrono::system_clock::now();
+        lastDelta = currentTimePoint - lastTimePoint;
+        lastTimePoint = currentTimePoint;
+}
+
+
+void Analyzer::setLastDelta( float lastDelta )
+{
+        this->lastDelta = std::chrono::duration< float >( lastDelta );
+}
+
+
+float Analyzer::getLastDelta()
+{
+        return lastDelta.count();
+}

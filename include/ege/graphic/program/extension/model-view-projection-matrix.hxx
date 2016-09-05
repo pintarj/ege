678bcf33b898a8c05353df33d1ea78ffbcf982cb
Janez Pintar <pintarj.zero@gmail.com>
2016-09-05 18:18:41 +0200

    mvp matrix extension for program

---
+++ include/ege/graphic/program/extension/model-view-projection-matrix.hxx
@@ -0,0 +1,33 @@
+
+
+#ifndef EGE_GRAPHIC_PROGRAM_EXTENSION_MODEL_VIEW_PROJECTION_MATRIX_HXX
+#define EGE_GRAPHIC_PROGRAM_EXTENSION_MODEL_VIEW_PROJECTION_MATRIX_HXX
+
+
+#include <ege/graphic/program/extension.hxx>
+
+
+namespace ege
+{
+        namespace graphic
+        {
+                namespace program
+                {
+                        namespace extension
+                        {
+                                class ModelViewProjectionMatrix: Extension
+                                {
+                                        private:
+                                                size_t matrixLocation;
+
+                                        protected:
+                                                ModelViewProjectionMatrix( Program& program, const char* mvpMatrixName );
+                                                void uniformMatrix( const float* matrix );
+                                };
+                        }
+                }
+        }
+}
+
+
+#endif

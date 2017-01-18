bd3a1bfbaaa4aaf801cd587aa9986ca4b3553867
Janez Pintar <pintarj.zero@gmail.com>
2017-01-18 20:48:26 +0100

    version header documented

--- include/ege/version.hxx
+++ include/ege/version.hxx
@@ -1,21 +1,36 @@
 
-
 #ifndef EGE_VERSION_HXX
 #define EGE_VERSION_HXX
 
+/**
+ * \namespace ege::version
+ * \brief Contains function to retrieve the library version.
+ * */
 
-#define EGE_VERSION_MAJOR       ( 0 )
-#define EGE_VERSION_MINOR       ( 1 )
-#define EGE_VERSION_BUILD       ( 2 )
-
+#define EGE_VERSION_MAJOR   (0)
+#define EGE_VERSION_MINOR   (1)
+#define EGE_VERSION_BUILD   (2)
 
 namespace ege
 {
-        namespace version
-        {
-                void get( unsigned* major, unsigned* minor, unsigned* build );
-        }
+    namespace version
+    {
+        /**
+         * \brief Retrieve the version number of the library.
+         *
+         * The version number will be stored in unsigned variables referenced by pointers passed as parameter.
+         * If "nullptr" (aka NULL) is passed to any parameter, that respective version number wouldn't be retrieved. \n
+         * Example of use, that retrieves only the "major" and "minor" version (without the "build"):
+         * \code
+         * unsigned major, minor;
+         * ege::version::get(&major, &minor, nullptr);
+         * \endcode
+         * \param[out] major The pointer to a variable where the "major" version number will be stored.
+         * \param[out] minor The pointer to a variable where the "minor" version number will be stored.
+         * \param[out] build The pointer to a variable where the "build" version number will be stored.
+         * */
+        void get(unsigned* major, unsigned* minor, unsigned* build);
+    }
 }
-
 
 #endif
--- source/version.cxx
+++ source/version.cxx
@@ -1,14 +1,19 @@
 #include <ege/version.hxx>
 
-
-void ege::version::get( unsigned* major, unsigned* minor, unsigned* build )
+namespace ege
 {
-        if ( major != nullptr )
+    namespace version
+    {
+        void get(unsigned* major, unsigned* minor, unsigned* build)
+        {
+            if (major != nullptr)
                 *major = EGE_VERSION_MAJOR;
 
-        if ( minor != nullptr )
+            if (minor != nullptr)
                 *minor = EGE_VERSION_MINOR;
 
-        if ( build != nullptr )
+            if (build != nullptr)
                 *build = EGE_VERSION_BUILD;
+        }
+    }
 }

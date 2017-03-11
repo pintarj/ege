971a8f2d4039b9acdf37c3de000a6249c01cf521
Janez Pintar <pintarj.zero@gmail.com>
2017-03-11 12:33:04 +0100

    EventUpdate fragment created

---
+++ include/private/ege/flow/event-update-fragment.hxx
@@ -0,0 +1,39 @@
+
+#ifndef EGE_FLOW_EVENTUPDATEFRAGMENT_HXX
+#define EGE_FLOW_EVENTUPDATEFRAGMENT_HXX
+
+#include <ege/flow/fragment.hxx>
+#include <private/ege/engine/control-thread.hxx>
+
+namespace ege
+{
+    namespace flow
+    {
+        class EventUpdate: public Fragment
+        {
+            private:
+                /**
+                 * \brief Stores reference to the control thread.
+                 * */
+                engine::ControlThread& controlThread;
+
+            protected:
+                /**
+                 * \brief Update events.
+                 * \param frame The frame that is updating.
+                 * */
+                virtual void performUpdate(const Frame& frame);
+
+            public:
+                /**
+                 * \brief Create an event update fragment, that fetch current scene from specified control thread.
+                 * \param controlThread The specified control thread.
+                 * */
+                EventUpdate(engine::ControlThread& controlThread);
+
+                virtual ~EventUpdate() {}
+        };
+    }
+}
+
+#endif
---
+++ source/flow/event-update-fragment.cxx
@@ -0,0 +1,25 @@
+#include <private/ege/flow/event-update-fragment.hxx>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <private/ege/engine/resources.hxx>
+
+namespace ege
+{
+    namespace flow
+    {
+        EventUpdate::EventUpdate(engine::ControlThread& controlThread):
+            controlThread(controlThread)
+        {
+
+        }
+
+        void EventUpdate::performUpdate(const Frame& frame)
+        {
+            glfwPollEvents();
+            glfwPollEvents(); // solve bug: glfw perform a key repressed after key repeating
+
+            if (engine::getGLFWWindow().shouldClose())
+                controlThread.getCurrentScene().shouldClose();
+        }
+    }
+}

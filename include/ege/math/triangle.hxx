9b25f84497792b579ad0f6ba153f9c59b37ff5e4
Janez Pintar <pintarj.zero@gmail.com>
2017-03-12 18:15:50 +0100

    math::Triangle documented

--- include/ege/math/triangle.hxx
+++ include/ege/math/triangle.hxx
@@ -1,33 +1,55 @@
 
-
 #ifndef EGE_MATH_TRIANGLE_HXX
 #define EGE_MATH_TRIANGLE_HXX
 
-
 #include <ege/math/vector.hxx>
-
 
 namespace ege
 {
-        namespace math
+    namespace math
+    {
+        /**
+         * \brief A mathematical triangle (represented by 3 vectors).
+         * \param T The type of the vector's components.
+         * */
+        template<typename T = float>
+        class Triangle
         {
-                template < typename T = float >
-                class Triangle
-                {
-                        private:
-                                Vector< 3, T > vertices[ 3 ];
+            private:
+                /**
+                 * \brief The vectors representing the triangle.
+                 * */
+                Vector<3, T> vertices[3];
 
-                        public:
-                                Triangle( Vector< 3, T > const& v0, Vector< 3, T > const& v1, Vector< 3, T > const& v2 );
-                                Vector< 3, T >& operator [] ( unsigned index );
-                                Vector< 3, T > const& operator [] ( unsigned index ) const;
-                                Vector< 3, T > normal( bool clockWise = true ) const;
-                };
-        }
+            public:
+                /**
+                 * \brief Creates a triangle.
+                 * \param v0, v1, v2 The vectors that define the triangle.
+                 * */
+                Triangle(const Vector<3, T>& v0, const Vector<3, T>& v1, const Vector<3, T>& v2) noexcept;
+
+                /**
+                 * \brief Access a vector of the triangle at the specific index.
+                 * \param index The index.
+                 * */
+                Vector<3, T>& operator [](unsigned index) noexcept;
+
+                /**
+                 * \brief Access a vector of the triangle at the specific index.
+                 * \param index The index.
+                 * */
+                const Vector<3, T>& operator [](unsigned index) const noexcept;
+
+                /**
+                 * \brief Calculates the normal of the triangle.
+                 * \param clockWise If it's \c true the vectors are treated to be in clockwise order,
+                 *     if \c false then are treated to be in counter-clockwise order. Default is \c false.
+                 * */
+                Vector<3, T> normal(bool clockWise = false) const noexcept;
+        };
+    }
 }
 
-
 #include "triangle.txx"
-
 
 #endif
--- include/ege/math/triangle.txx
+++ include/ege/math/triangle.txx
@@ -1,32 +1,35 @@
+#include <ege/math/triangle.hxx>
 
-
-template < typename T >
-ege::math::Triangle< T >::Triangle( Vector< 3, T > const& v0, Vector< 3, T > const& v1, Vector< 3, T > const& v2 ):
-        vertices( { v0, v1, v2 } )
+namespace ege
 {
+    namespace math
+    {
+        template<typename T>
+        Triangle<T>::Triangle(Vector<3, T> const& v0, Vector<3, T> const& v1, Vector<3, T> const& v2) noexcept:
+            vertices({v0, v1, v2})
+        {
 
-}
+        }
 
+        template<typename T>
+        Vector<3, T>& Triangle<T>::operator [](unsigned index) noexcept
+        {
+            return vertices[index];
+        }
 
-template < typename T >
-ege::math::Vector< 3, T >& ege::math::Triangle< T >::operator [] ( unsigned index )
-{
-        return vertices[ index ];
-}
+        template<typename T>
+        Vector<3, T> const& Triangle<T>::operator [](unsigned index) const noexcept
+        {
+            return vertices[index];
+        }
 
-
-template < typename T >
-ege::math::Vector< 3, T > const& ege::math::Triangle< T >::operator [] ( unsigned index ) const
-{
-        return vertices[ index ];
-}
-
-
-template < typename T >
-ege::math::Vector< 3, T > ege::math::Triangle< T >::normal( bool clockWise ) const
-{
-        if ( clockWise )
-                return ege::math::vector::crossProduct( vertices[ 2 ] - vertices[ 0 ], vertices[ 1 ] - vertices[ 0 ] ).normalized();
-        else
-                return ege::math::vector::crossProduct( vertices[ 1 ] - vertices[ 0 ], vertices[ 2 ] - vertices[ 0 ] ).normalized();
+        template<typename T>
+        Vector<3, T> Triangle<T>::normal(bool clockWise) const noexcept
+        {
+            if (clockWise)
+                return crossProduct(vertices[2] - vertices[0], vertices[1] - vertices[0]).normalized();
+            else
+                return crossProduct(vertices[1] - vertices[0], vertices[2] - vertices[0]).normalized();
+        }
+    }
 }
